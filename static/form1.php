a79ee88f384395adf2af422ba2a468c64317b3a7
ramon marmol <ramom85@gmail.com>
2019-02-22 02:54:14 -0500

    phpchange

---
+++ static/form1.php
@@ -0,0 +1,38 @@
+<?php 
+$errors = '';
+$myemail = 'rmarmol85@gmail.com';
+if(empty($_POST['name'])  ||
+   empty($_POST['email']) ||
+   empty($_POST['age']) ||
+   empty($_POST['phone']) ||
+   empty($_POST['textarea']))
+   
+{
+    $errors .= "\n Error: all fields are required";
+}
+$name = $_POST['name'];
+$email_address = $_POST['email'];
+$age = $_POST['age'];
+$phone= $_POST['phone'];
+$textarea= $_POST['textarea'];
+
+if (!preg_match(
+"/ ^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/i",
+$email_address))
+{
+    $errors .= "\n Error: Invalid email address";
+}
+if( empty($errors))
+{
+    $to = '$myemail';
+    $email_subject = "Contact form submission: $name";
+    $email_body = "You have received a new message. ".
+        " Here are the details:\n Name: $name \n ".
+        "Email: $email_address\n Message \n $message";
+    $headers = "From: $myemail\n";
+    $headers .= "Reply-To: $email_address";
+    mail($to,$email_subject,$email_body,$headers);
+    //redirect to the 'thank you' page
+    header('Location: thankyou.html');
+}
+?>
